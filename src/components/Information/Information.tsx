eb928ac6984648733be47fc3f1c266106ef0c792
Taiyi <moved0311@gmail.com>
2023-08-08 23:49:24 +0800

    fix: fix images path

--- src/components/Information/Information.tsx
+++ src/components/Information/Information.tsx
@@ -12,7 +12,7 @@ const Information = () => {
 		<div className="mb-2">
 			<div className="flex items-center justify-between">
 				<div className="flex items-center">
-					<img className="rounded-full mr-2" alt="avatar" src="resume/assets/avatar.jpg" width={50} height={50} />
+					<img className="rounded-full mr-2" alt="avatar" src="/resume/assets/avatar.jpg" width={50} height={50} />
 					<span>江太一 (Tai Yi Jiang)</span>
 				</div>
 				<div>
@@ -22,15 +22,15 @@ const Information = () => {
 			</div>
 			<ul className="flex flex-col gap-2 mt-3 pl-3">
 				<li className='flex'>
-					<img alt="email-icon" src="resume/assets/email.png" width={25} height={25} className='mr-2'/>
+					<img alt="email-icon" src="/resume/assets/email.png" width={25} height={25} className='mr-2'/>
 					<a href="mailto:moved0311@gmail.com">moved0311@gmail.com</a>
 				</li>
 				<li className='flex'>
-					<img alt="github-icon" src="resume/assets/github.png" width={25} height={25} className='mr-2'/>
+					<img alt="github-icon" src="/resume/assets/github.png" width={25} height={25} className='mr-2'/>
 					<a href="https://github.com/moved0311">https://github.com/moved0311</a>
 				</li>
 				<li className='flex'>
-					<img alt="linkin-icon" src="resume/assets/linkedin.png" width={25} height={25} className='mr-2'/>
+					<img alt="linkin-icon" src="/resume/assets/linkedin.png" width={25} height={25} className='mr-2'/>
 					<a href="https://www.linkedin.com/in/jiang-taiyi/">https://www.linkedin.com/in/jiang-taiyi/</a>
 				</li>
 			</ul>
